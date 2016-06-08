22c5a6c805456a8d43886b133fddcc07e3d76e99
Casper Bollen <halcwb@gmail.com>
2016-06-08 19:50:01 +0200

    Added simple hello service

---
+++ src/Informedica.GenUnit.App/Scripts/Start.fsx
@@ -0,0 +1,22 @@
+﻿//---------------------------------------------------------------------
+#load "load-project-release.fsx"
+open System
+open Suave // always open suave
+open Suave.Successful
+open Suave.Http
+open Suave.Web // for config
+
+printfn "initializing script..."
+
+let config =
+    let port = System.Environment.GetEnvironmentVariable("PORT")
+    { defaultConfig with
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Verbose
+        bindings = [ (if port = null then HttpBinding.mkSimple HTTP "127.0.0.1" 3000
+                      else HttpBinding.mkSimple HTTP "0.0.0.0" (int32 port)) ] }
+
+printfn "starting webserver ..."
+
+let app = OK "Hello World!"
+
+startWebServer config app
