3b781c0c0e95b898eb8b0661b5d02ba71f04c922
Casper Bollen <halcwb@gmail.com>
2016-06-09 11:12:28 +0200

    Heroku ref problem

--- app.fsx
+++ app.fsx
@@ -1,9 +1,9 @@
-﻿#if BOOTSTRAP
-System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fsprojects/Paket/releases/download/2.40.9/paket.exe" in use wc = new System.Net.WebClient() in let tmp = System.IO.Path.GetTempFileName() in wc.DownloadFile(url, tmp); System.IO.File.Move(tmp,System.IO.Path.GetFileName url);;
-#r "paket.exe"
-Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
-#endif
+﻿//#if BOOTSTRAP
+//System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+//if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fsprojects/Paket/releases/download/2.40.9/paket.exe" in use wc = new System.Net.WebClient() in let tmp = System.IO.Path.GetTempFileName() in wc.DownloadFile(url, tmp); System.IO.File.Move(tmp,System.IO.Path.GetFileName url);;
+//#r "paket.exe"
+//Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
+//#endif
 
 //---------------------------------------------------------------------
 #I __SOURCE_DIRECTORY__
