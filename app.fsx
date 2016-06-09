2559cb2de1b1cb63344554cca583858129309925
Casper Bollen <halcwb@gmail.com>
2016-06-09 11:26:07 +0200

    Added references to root script

--- app.fsx
+++ app.fsx
@@ -6,17 +6,17 @@
 //#endif
 
 //---------------------------------------------------------------------
-#I __SOURCE_DIRECTORY__
-#r "packages/FSPowerPack.Core.Community/Lib/Net40/FSharp.PowerPack.dll"
-#r "packages/FSPowerPack.Linq.Community/Lib/Net40/FSharp.PowerPack.Linq.dll"
-#r "packages/FSPowerPack.Metadata.Community/Lib/Net40/FSharp.PowerPack.Metadata.dll"
-#r "packages/FSPowerPack.Parallel.Seq.Community/Lib/Net40/FSharp.PowerPack.Parallel.Seq.dll"
+#I "./packages"
+#r "FSPowerPack.Core.Community/Lib/Net40/FSharp.PowerPack.dll"
+#r "FSPowerPack.Linq.Community/Lib/Net40/FSharp.PowerPack.Linq.dll"
+#r "FSPowerPack.Metadata.Community/Lib/Net40/FSharp.PowerPack.Metadata.dll"
+#r "FSPowerPack.Parallel.Seq.Community/Lib/Net40/FSharp.PowerPack.Parallel.Seq.dll"
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.Numerics.dll"
-#r "packages/Suave/lib/net40/Suave.dll"
-//#r "packages/Informedica.GenUtils.Lib/lib/net45/Informedica.GenUtils.Lib.dll"
-#r "packages/Informedica.GenUnits.lib/lib/net45/Informedica.GenUnits.lib.dll"
+#r "Suave/lib/net40/Suave.dll"
+#r "Informedica.GenUtils.Lib/lib/net45/Informedica.GenUtils.Lib.dll"
+#r "Informedica.GenUnits.lib/lib/net45/Informedica.GenUnits.lib.dll"
 
 open System
 open Suave // always open suave
