26f36eaa418b81546bbea89e7cb51ae1b6e36e00
Casper Bollen <halcwb@gmail.com>
2016-06-09 10:59:56 +0200

    Heroku ref problem

--- app.fsx
+++ app.fsx
@@ -15,8 +15,8 @@ Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
 #r "System.dll"
 #r "System.Numerics.dll"
 #r "packages/Suave/lib/net40/Suave.dll"
-#r "packages/Informedica.GenUnits.lib/lib/net45/Informedica.GenUnits.lib.dll"
-//#r "packages/Informedica.GenUtils.Lib/lib/net45/Informedica.GenUtils.Lib.dll"
+//#r "packages/Informedica.GenUnits.lib/lib/net45/Informedica.GenUnits.lib.dll"
+#r "packages/Informedica.GenUtils.Lib/lib/net45/Informedica.GenUtils.Lib.dll"
 
 open System
 open Suave // always open suave
@@ -24,7 +24,7 @@ open Suave.Successful
 open Suave.Http
 open Suave.Web // for config
 
-open Informedica.GenUnits.Lib
+open Informedica.GenUtils.Lib
 
 printfn "initializing script..."
 
