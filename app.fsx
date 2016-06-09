7805f8322e22ff445244cf6886b81ffa27928356
Casper Bollen <halcwb@gmail.com>
2016-06-09 10:50:23 +0200

    Moved start of app to root script

--- app.fsx
+++ app.fsx
@@ -5,4 +5,33 @@ if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fs
 Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
 #endif
 
-#load "src/Informedica.GenUnit.App/Scripts/Start.fsx"
+//---------------------------------------------------------------------
+#load "src/Informedica.GenUnit.App/Scripts/load-project-release.fsx"
+
+open System
+open Suave // always open suave
+open Suave.Successful
+open Suave.Http
+open Suave.Web // for config
+
+open Informedica.GenUnits.Lib
+
+printfn "initializing script..."
+
+let msg =
+    let eq = "200 mg[Mass]/ml[Volume] * 2 ml[Volume]/hour[Time]"
+    let rs = eq |> Api.eval
+    sprintf "This applicaton can calculate this expression:</br> %s = %s" eq rs
+
+let config =
+    let port = System.Environment.GetEnvironmentVariable("PORT")
+    { defaultConfig with
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Verbose
+        bindings = [ (if port = null then HttpBinding.mkSimple HTTP "127.0.0.1" 3000
+                      else HttpBinding.mkSimple HTTP "0.0.0.0" (int32 port)) ] }
+
+printfn "starting webserver ..."
+
+let app = OK msg
+
+startWebServer config app
