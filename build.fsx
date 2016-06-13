1d909fd3f2bdc9a69af3855d50c7c229bb7f08a7
Casper Bollen <halcwb@gmail.com>
2016-06-13 17:27:06 +0200

    Changed npm file path

--- build.fsx
+++ build.fsx
@@ -17,7 +17,7 @@ open SourceLink
 let clientPath = "./client"
 
 Target "ClientTests" <| fun _ ->
-    let npmFilePath = environVarOrDefault "NPM_FILE_PATH" defaultNpmParams.NpmFilePath
+    let npmFilePath = environVarOrDefault "NPM_FILE_PATH" "npm"
     Npm <| fun p ->
         { p with
             Command = Install Standard
