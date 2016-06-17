8f64e14124b195351899f95bfa38004ab7fc7415
halcwb <halcwb@gmail.com>
2016-06-17 12:38:40 +0200

    One more travis node update try

--- install_travis.sh
+++ install_travis.sh
@@ -2,4 +2,4 @@
 
 sudo ~/.nvm/v0.10.36/bin/npm cache clean -f
 sudo ~/.nvm/v0.10.36/bin/npm install -g n
-sudo ~/home/travis/.nvm/v0.10.36/lib/node_modules/n/bin/n stable
+sudo ~/.nvm/v0.10.36/lib/node_modules/n/bin/n stable
