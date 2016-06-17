17abba75ca39d7b8d06d73b7ea4a032df94f22bb
Casper Bollen <halcwb@gmail.com>
2016-06-17 02:00:58 +0200

    Fix for npm problem

--- scripts/buildClient.fsx
+++ scripts/buildClient.fsx
@@ -6,14 +6,112 @@ open System
 open System.IO
 
 open Fake
-open Fake.NpmHelper
 
 let dir = (new DirectoryInfo(__SOURCE_DIRECTORY__)).Parent.FullName
 Environment.CurrentDirectory <- dir
     
-printfn "%s" Environment.CurrentDirectory
+let clientPath   = "./client"
+let nodeFilePath = "./packages/Node.js/node.exe"
+let npmFilePath  = "./packages/Npm/node_modules/npm/bin/npm-cli.js"
 
-let clientPath = "./client"
+/// Default paths to node
+let private nodeFileName = 
+    match isUnix with
+    | true -> ""
+    | _    -> nodeFilePath
+
+/// Default paths to Npm
+let private npmFileName =
+    match isUnix with
+    | true -> "/usr/local/bin/npm"
+    | _    -> npmFilePath
+
+/// Arguments for the Npm install command
+type InstallArgs =
+| Standard
+| Forced
+
+/// The list of supported Npm commands. The `Custom` alternative
+/// can be used for other commands not in the list until they are
+/// implemented
+type NpmCommand =
+| Install of InstallArgs
+| Run of string
+| Custom of string
+
+/// The Npm parameter type
+type NpmParams = 
+    { Src: string
+      NodeFilePath: string
+      NpmFilePath: string
+      WorkingDirectory: string
+      Command: NpmCommand
+      Timeout: TimeSpan }
+
+/// Npm default parameters
+let defaultNpmParams = 
+    { Src = ""
+      NodeFilePath = nodeFileName
+      NpmFilePath = npmFileName
+      Command = Install Standard
+      WorkingDirectory = "."
+      Timeout = TimeSpan.MaxValue }
+
+let private parseInstallArgs = function
+    | Standard -> ""
+    | Forced   -> " --force"
+
+let private parse = function
+    | Install installArgs -> sprintf "install%s" (installArgs |> parseInstallArgs)
+    | Run str -> sprintf "run %s" str
+    | Custom str -> str
+
+let run npmParams =
+    let npmPath = Path.GetFullPath(npmParams.NpmFilePath)
+    let nodePath = 
+        if npmParams.NodeFilePath |> String.IsNullOrEmpty then ""
+        else Path.GetFullPath(npmParams.NodeFilePath)
+    let fileName = 
+        if nodePath |> String.IsNullOrEmpty then npmPath else nodePath
+    let arguments = 
+        let args = npmParams.Command |> parse
+        if nodePath |> String.IsNullOrEmpty then args
+        else sprintf "%s %s" npmPath args
+    let workingDir = Path.GetFullPath(npmParams.WorkingDirectory)
+
+    let ok = 
+        execProcess (fun info ->
+            info.FileName <- fileName
+            info.WorkingDirectory <- workingDir
+            info.Arguments <- arguments) npmParams.Timeout
+    if not ok then failwith (sprintf "'npm %s' task failed" arguments)
+
+/// Runs npm with the given modification function. Make sure to have npm installed,
+/// you can install npm with nuget or a regular install. To change which `Npm` executable
+/// to use you can set the `NpmFilePath` parameter with the `setParams` function.
+///
+/// ## Parameters
+///
+///  - `setParams` - Function used to overwrite the Npm default parameters.
+///
+/// ## Sample
+///
+///        Target "Web" (fun _ ->
+///            Npm (fun p ->
+///                   { p with
+///                       Command = Install Standard
+///                       WorkingDirectory = "./src/FAKESimple.Web/"
+///                   })
+///
+///            Npm (fun p ->
+///                   { p with
+///                       Command = (Run "build")
+///                       WorkingDirectory = "./src/FAKESimple.Web/"
+///                   })
+///        )
+let Npm setParams =
+    defaultNpmParams |> setParams |> run
+
 
 let buildClient () =
     let npmFilePath = environVarOrDefault "NPM_FILE_PATH" defaultNpmParams.NpmFilePath
--- src/GenUnitApp/Program.fs
+++ src/GenUnitApp/Program.fs
@@ -6,15 +6,21 @@ open Suave.Successful
 open Suave.Http
 open Suave.Web // for config
 
+open Informedica.GenUtils.Lib.BCL
 open GenUnitApp
 
+let iif x1 x2 = if x1 |> String.IsNullOrEmpty |> not then x1 else x2
+    
 
 [<EntryPoint>]
 let main argv =
     
     printfn "initializing script..."
 
-    let home = AppDomain.CurrentDomain.RelativeSearchPath
+    let home = 
+        iif AppDomain.CurrentDomain.RelativeSearchPath 
+            AppDomain.CurrentDomain.BaseDirectory 
+        |> iif Environment.CurrentDirectory
 
     match argv |> List.ofArray with
     | p::_ -> Server.start home p
