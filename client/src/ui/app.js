aef55d3e52255b1ebfc52899282f2194d29d605c
halcwb <halcwb@gmail.com>
2016-06-18 22:02:06 +0200

    First webix impl

--- client/src/ui/app.js
+++ client/src/ui/app.js
@@ -1,14 +1,18 @@
 /**
  * Created by halcwb on 17/06/16.
  */
+/*global webix, console*/
 
 "use strict";
 
 (function () {
     var debug = require("debug")("app");
-    var get_msg = require("../../spike/ajax/get_simple_msg.js");
+    var msg = require("../lib/ajax/request.js").getMessage();
 
     debug("Starting the app!");
-    get_msg.getMessage();
+    msg.then(function (resp) {
+        debug(resp);
+        webix.alert(resp.text());
+    });
 
 })();
