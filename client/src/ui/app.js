34f197a34fe1370ac9da814567620504577e4f4e
Casper Bollen <halcwb@gmail.com>
2016-07-04 15:44:14 +0200

    feat: calculate expression gui

--- client/src/ui/app.js
+++ client/src/ui/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by halcwb on 17/06/16.
  */
-/*global webix, console, app, debug */
+/*global webix, $$, console, app, debug */
 
 "use strict";
 
@@ -11,12 +11,41 @@ webix.ready(function () {
     var reload = require("../lib/util/reload.js");
     var msg = require("../lib/ajax/request.js").getMessage();
 
-    // Starting reload for development
+    // handles the calulcate button click
+    var onClickCaclulate = function () {
+      var text = $$('expression_text').getValue();
+      debug('going to calcluate', text);
+      $$('result_template').setHTML(text);
+    };
+
+    // starting reload for development
     reload.init();
 
     debug("Starting the app!, ok");
     msg.then(function (resp) {
         webix.alert(resp.text());
     });
+
+    // create the ui
+    webix.ui({
+        rows: [
+            { type: 'header', template: 'GenUnitApp' },
+            { cols: [
+                { view: "form", id: 'expression_form', elements: [
+                    { view:"text",
+                      id: 'expression_text',
+                      placeholder:"<expression>",
+                      label: 'expression' },
+                    { view:"button",
+                      id: 'expression_button',
+                      value: 'calculate' }
+                ]},
+                { template: 'result', id: 'result_template' }
+            ]}
+        ]
+    });
+
+    // attach events
+    $$('expression_button').attachEvent('onItemClick', onClickCaclulate);
 
 });
