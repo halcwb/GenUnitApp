7e36c2d62bcc828469d8f429011d3df2d7b3876f
Casper Bollen <halcwb@gmail.com>
2016-07-05 01:59:41 +0200

    docs: release commands

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,7 +1,10 @@
+#### 0.1.0-alpha - July 5 2016
+* First working client setup
+* First implemented feature: evaluate an expression
+
 #### 0.0.3-alpha - June 18 2016
 * First working client setup
 * Just shows a message box with a GenUnit result
-
 
 #### 0.0.2-alpha - June 06 2016
 * First working server setup
--- src/GenUnitApp/AssemblyInfo.fs
+++ src/GenUnitApp/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("GenUnitApp")>]
 [<assembly: AssemblyCompanyAttribute("halcwb")>]
 [<assembly: AssemblyDescriptionAttribute("Client Server App to calculate and convert units")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.3"
-    let [<Literal>] InformationalVersion = "0.0.3"
+    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] InformationalVersion = "0.1.0"
