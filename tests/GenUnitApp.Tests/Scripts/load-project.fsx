b9416b8b85d13486c4abe71c716a8135a0ce52b9
Casper Bollen <halcwb@gmail.com>
2016-07-07 23:35:01 +0200

    chore: add references for fsi

--- tests/GenUnitApp.Tests/Scripts/load-project.fsx
+++ tests/GenUnitApp.Tests/Scripts/load-project.fsx
@@ -1,7 +1,4 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
 #load "load-references.fsx"
-#load "../../../paket-files/halcwb/YoLo/YoLo.fs"
-      "../AssemblyInfo.fs"
-      "../Server.fs"
-      "../Program.fs"
+#load "../BuildTest.fs"
