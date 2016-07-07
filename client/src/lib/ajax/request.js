a430aca42129eddde9bb1020f88e46a4c95bb053
Casper Bollen <halcwb@gmail.com>
2016-07-07 16:06:13 +0200

    docs: added javascript docs

--- client/src/lib/ajax/request.js
+++ client/src/lib/ajax/request.js
@@ -8,6 +8,8 @@
 
     var debug = app.debug('client:request');
 
+    /// Get a message just to test that
+    /// the server is life.
     exports.getMessage = function () {
         debug('request message');
         return webix.ajax().post("/msg");
