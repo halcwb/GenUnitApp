0870f7a833a9247afa34009e6f7f26a3e6305288
Casper Bollen <halcwb@gmail.com>
2016-07-19 16:01:40 +0200

    fix: multiple ajax posts in tests

--- client/tests/smoke/process_request_tests.js
+++ client/tests/smoke/process_request_tests.js
@@ -9,12 +9,24 @@ describe("Smoke process request tests", function () {
     "use strict";
 
     var HOME_URL = "http://localhost:3000/request";
+    var test1, test2, test3;
 
+
+    before(function () {
+        var req = { act: 'test1', qry: {}};
+
+        console.log('running before ');
+        test1 = webix.ajax().post(HOME_URL, JSON.stringify(req));
+        req.act = "test2";
+        test2 = webix.ajax().post(HOME_URL, JSON.stringify(req));
+        req.act = "evalute";
+        req.qry = { expr: "20 mL[Volume] * 100 mg[Mass]/ml[Volume]"};
+        test3 = webix.ajax().post(HOME_URL, JSON.stringify(req));
+
+    });
 
     it("should echo the test request with response", function (done) {
-        var test = { act: 'test1', qry: {} };
-
-        var promise = webix.ajax().post(HOME_URL, JSON.stringify(test));
+        var promise = test1;
 
         console.log('test1');
         promise.then(function (resp) {
@@ -31,9 +43,7 @@ describe("Smoke process request tests", function () {
 
 
     it("should echo the test request with response 2", function (done) {
-        var test = { act: 'test2', qry: {} };
-
-        var promise = webix.ajax().post(HOME_URL, JSON.stringify(test));
+        var promise = test2;
 
         console.log('test2');
         promise.then(function (resp) {
@@ -50,10 +60,7 @@ describe("Smoke process request tests", function () {
 
 
     it ("should be able to request an evaluation of an expression", function (done) {
-        var req = { act: "evaluate", qry: { expr: "20 mL[Volume] * 100 mg[Mass]/ml[Volume]"}};
-
-        var promise = webix.ajax().post(HOME_URL, JSON.stringify(req));
-
+        var promise = test3;
 
         console.log('test3');
         promise.then(function (resp) {
