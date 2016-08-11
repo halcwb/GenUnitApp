d3aea9a637a1f26e9a8b2b3fdbc4483081c0f5a5
Casper Bollen <halcwb@gmail.com>
2016-08-11 12:11:36 +0200

    feat: server side groups and get units from combined groups

--- src/GenUnitApp/GenUnits.fs
+++ src/GenUnitApp/GenUnits.fs
@@ -5,8 +5,14 @@ module GenUnits =
     
     module Api =
 
+        open Informedica.GenUtils.Lib.BCL
         open Informedica.GenUnits.Lib
-    
+        open Informedica.GenUnits.Lib.Unit.Units
+
+        module CU = CombiUnit
+        module VU = ValueUnit
+        module UG = UnitGroup    
+
         let fromString = Api.fromString
         
         let toString = Api.toString
@@ -16,15 +22,30 @@ module GenUnits =
         let convert = Api.convert
         
         let getUnits grp = 
-            Unit.Units.units
-            |> List.collect id
-            |> List.filter (fun u -> 
-                (u 
-                |> Unit.getGroupName 
-                |> Unit.Name.toString = grp) || (grp |> String.isEmpty))
-            |> List.map (fun u -> 
-                let abbr = u.Abbreviation |> fst |> Unit.Name.toString
-                let grp = u.Group |> Unit.Name.toString
-                sprintf "%s[%s]" abbr grp)
+            if grp |> String.isNullOrWhiteSpace then 
+                units
+                |> List.collect id
+                |> List.filter (fun u -> 
+                    (u 
+                    |> Unit.getGroupName 
+                    |> Unit.Name.toString = grp) || (grp |> String.isEmpty))
+                |> List.map (fun u -> 
+                    let abbr = u.Abbreviation |> fst |> Unit.Name.toString
+                    let grp = u.Group |> Unit.Name.toString
+                    sprintf "%s[%s]" abbr grp)
+
+            else
+                grp
+                |> String.split "/"
+                |> List.map UG.fromString
+                |> List.reduce (/)
+                |> UG.getUnits
+                |> List.map CU.toString
+
+        let groups =
+            units
+            |> List.collect (fun ul ->
+                ul |> List.map (fun u -> u.Group |> Unit.Name.toString)
+            )
 
 
